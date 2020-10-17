b1b2c9d689f94344b5a615e83f80ed04b62eedc7
TabanschiAlex <talex.alexandr@gmail.com>
2020-10-17 18:41:15 +0300

    Валидация точки адаптивный шрифт дисплея

--- dist/Calculator.js
+++ dist/Calculator.js
@@ -14,41 +14,56 @@ var Calculator = /** @class */ (function () {
         this.memory = '';
         return this.showValueOnDisplay();
     };
+    Calculator.prototype.checkFontSize = function () {
+        if (this.memory.length < 10) {
+            this.display.style.fontSize = '90px';
+        }
+        if (this.memory.length >= 10) {
+            this.display.style.fontSize = '40px';
+        }
+        if (this.memory.length > 25) {
+            this.display.style.fontSize = '20px';
+        }
+    };
     Calculator.prototype.calculate = function (value) {
+        this.checkFontSize();
         if (value === '=') {
-            this.memory = eval(this.memory);
-            return this.showValueOnDisplay();
+            this.memory = eval(this.memory).toString();
         }
         if (this.validate(value) === true) {
             this.memory += value;
-            return this.showValueOnDisplay();
         }
-        if (this.validate(value) === false) {
-            return this.showValueOnDisplay();
-        }
+        return this.showValueOnDisplay();
     };
     Calculator.prototype.validate = function (value) {
         var operators = ['+', '-', '*', '/'];
+        var lastOperator;
+        if (value === '=') {
+            return false;
+        }
         if (!this.memory) {
             if (operators.indexOf(value) != -1 || value === '.') {
                 return false;
             }
-            ;
             return true;
         }
         if (operators.indexOf(this.memory.slice(-1)) != -1 && operators.indexOf(value) != -1) {
             this.memory = this.memory.slice(0, this.memory.length - 1);
             return true;
         }
-        if (value === '.' && this.memory.slice(operators.indexOf(this.memory) + 1).indexOf('.') === -1) {
-            this.memory = this.memory.slice(operators.indexOf(this.memory) + 1);
-            console.log(this.memory);
-            return true;
+        if (value === '.') {
+            lastOperator = this.memory;
+            for (var i = 0; i < operators.length; i++) {
+                lastOperator = lastOperator.slice(lastOperator.lastIndexOf(operators[i]) + 1);
+            }
+            if (lastOperator.indexOf('.') != -1) {
+                return false;
+            }
         }
         return true;
     };
     return Calculator;
 }());
 var calculator = new Calculator();
-console.log(calculator.showValueOnDisplay());
+calculator.showValueOnDisplay();
 //# sourceMappingURL=Calculator.js.map
--- src/Calculator.ts
+++ src/Calculator.ts
@@ -21,29 +21,46 @@ class Calculator {
         return this.showValueOnDisplay();
     }
 
+    private checkFontSize(): void {
+        if (this.memory.length < 10) {
+            this.display.style.fontSize = '90px';
+        }
+
+        if (this.memory.length >= 10) {
+            this.display.style.fontSize = '40px';
+        }
+
+        if (this.memory.length > 25) {
+            this.display.style.fontSize = '20px';
+        }
+    }
+
     public calculate(value: string): string {
+        this.checkFontSize();
+
         if (value === '=') {
-            this.memory = eval(this.memory);
-            return this.showValueOnDisplay();
+            this.memory = eval(this.memory).toString();
         }
 
         if (this.validate(value) === true) {
             this.memory += value;
-            return this.showValueOnDisplay();
         }
 
-        if (this.validate(value) === false) {
-            return this.showValueOnDisplay();
-        }
+        return this.showValueOnDisplay();
     }
 
     private validate(value: string): boolean {
         const operators: string[] = ['+', '-', '*', '/'];
+        let lastOperator: string;
+
+        if (value === '=') {
+            return false;
+        }
 
         if (!this.memory) {
             if (operators.indexOf(value) != -1 || value === '.') {
                 return false;
-            };
+            }
 
             return true;
         }
@@ -53,17 +70,21 @@ class Calculator {
             return true;
         }
 
-        if (value === '.' && this.memory.slice(operators.indexOf(this.memory) + 1).indexOf('.') === -1)  {
-            this.memory = this.memory.slice(operators.indexOf(this.memory) + 1);
-            console.log(this.memory)
-            return true;
+        if (value === '.') {
+            lastOperator = this.memory;
+
+            for (let i: number = 0; i < operators.length; i++) {
+                lastOperator = lastOperator.slice(lastOperator.lastIndexOf(operators[i]) + 1);
+            }
+
+            if (lastOperator.indexOf('.') != -1)  {
+                return false;
+            }
         }
 
         return true;
     }
-
-
 }
 
 const calculator = new Calculator();
-console.log(calculator.showValueOnDisplay());
+calculator.showValueOnDisplay();
